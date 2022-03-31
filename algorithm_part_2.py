f40fc4a5690d37be5f001dbc97eff298b8ef118a
kyle.macasinag <kylenmac@verizon.net>
2022-03-31 16:17:01 -0400

    Un-commented everything

--- algorithm_part_2.py
+++ algorithm_part_2.py
@@ -1,32 +1,32 @@
 #Number 1:
 
-# def switcheroo(word):
-#     reversal = word[::-1]  
-#     return reversal
+def switcheroo(word):
+    reversal = word[::-1]  
+    return reversal
 
-# print(switcheroo("Jiu Jitsu"))
+print(switcheroo("Jiu Jitsu"))
 
 #Number 2:
 
-# def words_need_capital(needs_upper_case): 
-#     result = needs_upper_case.title()
-#     print(result)
+def words_need_capital(needs_upper_case): 
+    result = needs_upper_case.title()
+    print(result)
 
 
 
-# words_need_capital("ciaphas cain")
+words_need_capital("ciaphas cain")
 
 #number 3:
 
-# def palindrome_checker(palimino): 
-#     reversal = palimino [::-1]
-#     to_compare = reversal.lower()
-#     original_to_compare= palimino.lower()
-#     if original_to_compare == to_compare:
-#         print("It's a palindrome!")
-#     elif original_to_compare != to_compare:
-#         print("It's not a palindrome")
+def palindrome_checker(palimino): 
+    reversal = palimino [::-1]
+    to_compare = reversal.lower()
+    original_to_compare= palimino.lower()
+    if original_to_compare == to_compare:
+        print("It's a palindrome!")
+    elif original_to_compare != to_compare:
+        print("It's not a palindrome")
 
 
-# palindrome_checker("Madam")
+palindrome_checker("Madam")
 
